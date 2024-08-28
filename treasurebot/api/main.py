f6d4acc5ab5ab5d73bbac4bfc458ad1164a58c22
Muhammad Nouman <mnouman2603@gmail.com>
2024-08-28 14:23:04 +0200

    absolute paths changed to relative paths

--- treasurebot/api/main.py
+++ treasurebot/api/main.py
@@ -4,8 +4,7 @@ import numpy as np  # Import NumPy for array operations
 from treasurebot.ml_logic.registry import get_model
 from treasurebot.ml_logic.preprocessor import preprocess_image
 from treasurebot.ml_logic.data import get_picture
-
-
+import os
 
 
 # Load environment variables
@@ -16,16 +15,21 @@ app.state.model = get_model()
 
 @app.get("/classify")
 async def classify_image():
-    # Open the image from the upload
-    image = get_picture('test.jpg')
+    # Get the directory of the current script
+    current_dir = os.path.dirname(__file__)
+    # moving to the root directory
+    parent_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+    # Construct the path to the 'test.jpg' file
+    image_path = os.path.join(parent_dir, 'pictures', 'test.jpg')
 
     # Preprocess the image using the defined function
-    preprocessed_image = preprocess_image(image)
-
+    #preprocessed_image = preprocess_image('/home/muhammad/code/Muhammad-Nou/TreasureBot3000/pictures/test.jpg')
+    preprocessed_image = preprocess_image(image_path)
     # Make a prediction using your model (replace with your model call)
     prediction = app.state.model.predict(preprocessed_image)
 
-    return {"prediction": prediction}
+    return {"prediction": str(prediction)}
+print(classify_image())
 #
 @app.get('/')
 def root():
