db0f249394aa96bc8d95d5ae25c52d1ea8f5000f
farah khalifa <farah.k@hotmail.de>
2024-08-29 14:47:06 +0200

    changed models name

--- treasurebot/ml_logic/registry.py
+++ treasurebot/ml_logic/registry.py
@@ -5,6 +5,6 @@ from tensorflow.keras.models import load_model
 def get_model():
 
     current_dir = os.path.dirname(__file__)
-    model_path = os.path.abspath(os.path.join(current_dir, '..', 'models', 'model_v2.h5'))
+    model_path = os.path.abspath(os.path.join(current_dir, '..', 'models', 'model_v3.h5'))
     model = load_model(model_path)
     return model
