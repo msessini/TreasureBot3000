dd676cae4712f3bab417f5e8049c3d7579a0b5f2
mario sessini <mario.sessini@gmail.com>
2024-09-02 17:48:49 +0200

    monday

--- treasurebot/ml_logic/model.py
+++ treasurebot/ml_logic/model.py
@@ -3,5 +3,5 @@ import numpy as np
 classes = ['Aluminium', 'DrinkCans', 'Glass', 'GlassBottles', 'Organic', 'Paperboard', 'Plastic', 'PlasticBottles']
 
 def get_label(prediction):
-
+    print(prediction)
     return classes[np.argmax(prediction)]
